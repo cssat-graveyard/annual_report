489de873f91ae454a5d25ecd443539830297a22f
Gregor Passolt <gregorp@uw.edu>
2014-12-19 09:41:04 -0800

    start of sparkline set up

--- sparklines.R
+++ sparklines.R
@@ -1,6 +1,11 @@
-setwd("S:/Data Portal/annual_report")
+#setwd("S:/Data Portal/annual_report")
 load("graph_dat.RData")
 
 measures <- list()
 
 sections <- c("Safety", "Permanency", "Well-Being")
+
+# Columns for table: 
+# Section | Table | Measure | Units | start_val | end_val | change_cat | assessment
+
+# Outline in google spreadsheet
