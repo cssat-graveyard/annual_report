13b91c2ce92c714b05683ea5e620114685c775ee
oieeri26 <oieeri26@gmail.com>
2015-05-18 16:29:56 -0700

    finished updating code for safety

---
+++ annual_report_stored_procedures/stored_procedures/rate_of_placements_by_prior_placements.R
@@ -0,0 +1,45 @@
+
+source("functions.R")
+
+###########################################
+# pRate of Placements by Prior Placements #
+###########################################
+
+placement_order <- sqlQuery(con_test_annie, "SELECT RO.*
+                            ,LC.old_region_cd
+                            FROM rate_placement_order_specific AS RO
+                            JOIN ref_lookup_county_region AS LC
+                            ON LC.county_cd = ro.county_cd;")
+
+# aggregating data to region level
+
+placement_order_agg <- placement_order %>% group_by(qry_type, date_type, cohort_date, nth_order, old_region_cd) %>%
+    summarize(cnt_nth_order_placement_cases = sum(cnt_nth_order_placement_cases),
+              cnt_prior_order_si_referrals = sum(cnt_prior_order_si_referrals),
+              placement_rate = sum(cnt_nth_order_placement_cases, na.rm = T)/sum(cnt_prior_order_si_referrals, na.rm = T) * 1000) %>%
+    filter(cohort_date > start_d & cohort_date < end_d)
+
+# loop to get ts data	
+
+dat2 <- as.data.frame(NULL)
+
+for(h in 0:6){	
+    d <- h
+    dat1 <- as.data.frame(NULL)
+    for (i in 1:3) {
+        dat <- ts_func(placement_order_agg, old_region_cd == d & nth_order == i, x = 8)
+        dat1 <- rbind.fill(dat1, dat)
+    }
+    dat2 <- rbind(dat2, dat1)					
+}									
+
+names(dat2) <- c("qry_type", "date_type", "cohort_date", "nth_order", "old_region_cd", "cnt_nth_order_placement_cases", "cnt_prior_order_si_referrals", "placement_rate", "seasonal", "trend")	
+
+for(i in 1:ncol(dat2)){
+    dat2[, i] <- class_convert(dat2[,i])
+}
+
+# loading data into mySQL
+# sqlDrop(con_test_annie, sqtable = "test_annie.rate_placement_order_specific_ts")
+sqlSave(con_test_annie, dat = dat2, tablename = "test_annie.rate_placement_order_specific_ts", rownames = FALSE)	
+
---
+++ annual_report_stored_procedures/stored_procedures/rate_of_substantiated_cases_of_maltreatment_while_in_care.R
@@ -0,0 +1,53 @@
+
+source("functions.R")
+
+#############################################################
+# Rate of Substantiated Cases of Maltreatment While in Care #
+#############################################################
+
+maltreatment <- sqlQuery(con_test_annie, "SELECT 
+                         M.*
+                         ,LC.old_region_cd
+                         FROM rate_care_day_maltreatment AS M
+                         JOIN ref_lookup_county_region AS LC
+                         ON M.county_cd = LC.county_cd
+                         ORDER BY 
+                         M.county_cd
+                         ,M.fiscal_yr asc;")
+
+# from the code Joe used, not sure where this came from and we don't have region data at the moment
+maltreatment <- rbind(maltreatment, c(0, 2, 2014, 0, 3099673, 196, 6.323247646, 0))
+
+maltreatment_agg <- maltreatment %>% group_by(date_type, qry_type, old_region_cd, fiscal_yr) %>%
+    summarize(cnt_incidents = sum(cnt_incidents),
+              care_days = sum(care_days),
+              care_day_incident_rate  = sum(cnt_incidents)/sum(care_days) * 100000)
+
+####################
+# testing function #
+####################
+
+lims(maltreatment, old_region_cd == 0, x1 = 7)
+
+##########################
+# function to get limits #
+##########################
+
+dat1 <- as.data.frame(NULL)
+
+for (i in 0:6) {
+    dat <- lims(maltreatment_agg, old_region_cd == i, x1 = 7)
+    dat1 <- rbind.fill(dat1, dat)
+}
+
+############################
+# preparing data for mySQL #
+############################
+
+for(i in 1:ncol(dat1)){
+    dat1[, i] <- class_convert(dat1[,i])
+}									
+
+# loading data into mySQL
+# sqlDrop(con_test_annie, sqtable = "test_annie.rate_care_day_maltreatment_limits")
+sqlSave(con_test_annie, dat = dat1, tablename = "test_annie.rate_care_day_maltreatment_limits", rownames = FALSE)
