0fa3eb0307bc7741c7bf6055e6f1cda64db13709
oieeri26 <oieeri26@gmail.com>
2015-05-20 14:58:26 -0700

    updated sql queires

--- annual_report_stored_procedures/stored_procedures/perm_adoption_query.sql
+++ annual_report_stored_procedures/stored_procedures/perm_adoption_query.sql
@@ -1,8 +1,6 @@
 declare @fystart int= 2000;
 declare @fystop int= 2014;
 
-
-
 if object_id('tempdb..##placements') is not null
 	drop table ##placements
 
@@ -63,6 +61,8 @@ select
 		,cast(null as int) as prior_year_service
 		,cast(null as int) as post_year_service
 		,rp_ep.cd_discharge_type
+		,convert(int, null) AS id_intake_fact
+		,convert(int, null) AS id_provider_dim_caregiver
 into ##placements
 from (select  distinct state_fiscal_yyyy sfy
 							,min(ID_CALENDAR_DIM)  over (partition by state_fiscal_yyyy order by ID_CALENDAR_DIM) fy_start_date_int
@@ -258,9 +258,9 @@ where anniv_removal_dt is not null and anniv_removal_dt NOT between plc_begin an
 ---- RUN THIS LINE SEPERATELY ----
 ----------------------------------
 
---add case id
-alter table ##placements
-add id_intake_fact int;
+----add case id
+--alter table ##placements
+--add id_intake_fact int;
 
 ----------------------------------
 
@@ -279,8 +279,8 @@ on
 ---- RUN THIS LINE SEPERATELY ----
 ----------------------------------
 
-alter table ##placements
-add id_provider_dim_caregiver int;
+--alter table ##placements
+--add id_provider_dim_caregiver int;
 
 ----------------------------------
 
@@ -295,19 +295,18 @@ join
 on
     ##placements.id_placement_fact = [base].[rptPlacement_Events].id_placement_fact
 
-
 ----------------------------------
 ---- RUN THIS LINE SEPERATELY ----
 ----------------------------------
 
-if object_id('tempdb..##tbl_sib_ep_cnt') is not null
+if object_id('tempdb..#tbl_sib_ep_cnt') is not null
   drop table ##tbl_sib_ep_cnt
 
 select 
     id_calendar_dim 
     ,id_intake_fact 
     ,count(id_intake_fact) cnt_id_intake
-into ##tbl_sib_ep_cnt
+into #tbl_sib_ep_cnt
 from calendar_dim cd
     join ##placements pl
         on cd.ID_CALENDAR_DIM between 
@@ -323,15 +322,15 @@ having count(id_intake_fact) > 1
 ---- RUN THIS LINE SEPERATELY ----
 ----------------------------------
 
-if object_id('tempdb..##tbl_sib_plc_cnt') is not null
-    drop table ##tbl_sib_plc_cnt
+if object_id('tempdb..#tbl_sib_plc_cnt') is not null
+    drop table #tbl_sib_plc_cnt
 
 select 
     id_calendar_dim 
     ,id_intake_fact 
     ,id_provider_dim_caregiver 
     ,count(id_intake_fact) cnt_id_intake
-into ##tbl_sib_plc_cnt
+into #tbl_sib_plc_cnt
 from CALENDAR_DIM cd
     join ##placements pl
         on cd.ID_CALENDAR_DIM between 
@@ -342,3 +341,88 @@ group by
     id_calendar_dim 
     ,id_intake_fact 
     ,id_provider_dim_caregiver 
+
+----------------------------------
+---- RUN THIS LINE SEPERATELY ----
+----------------------------------
+
+if object_id('tempdb..##tbl_sib_plc_tgh_cnt') is not null
+    drop table ##tbl_sib_plc_tgh_cnt
+
+select 
+    ec.id_calendar_dim 
+    ,ec.id_intake_fact
+    ,ec.cnt_id_intake cnt_id_intake_ec
+    ,max(pc.cnt_id_intake) max_cnt_id_intake_pc
+into ##tbl_sib_plc_tgh_cnt
+from #tbl_sib_ep_cnt ec
+    join #tbl_sib_plc_cnt pc
+        on ec.id_calendar_dim = pc.id_calendar_dim
+        and ec.id_intake_fact = pc.id_intake_fact 
+group by 
+    ec.id_calendar_dim 
+    ,ec.id_intake_fact
+    ,ec.cnt_id_intake
+order by ec.id_intake_fact 
+
+
+  if object_id('tempdb..##placement_prep17_5') is not null
+	drop table ##placement_prep17_5
+
+select 
+	child id_prsn
+	,removal_dt
+	,discharge_dt
+	,birthdate dt_birth
+	,dbo.fnc_datediff_yrs(birthdate, removal) age_removal
+	,dateadd(yy, 17.5, birthdate) dt_17_5
+	,dateadd(dd, 30, dateadd(yy, 17.5, birthdate)) dt_17_5_plus_30
+	,epf.id_prsn_child as plan_cntr
+	,epf.first_plan_act 
+	,max(pcad.cd_placement_care_auth) single_pca --not informative...just to stop dups
+	,state_fiscal_yyyy cohort_fy
+	,rp.cd_region
+into ##placement_prep17_5
+from base.rptPlacement rp
+	join placement_care_auth_fact pca
+		on child = pca.id_prsn 
+		and dateadd(yy, 17.5, birthdate) between 
+			dbo.IntDate_to_CalDate(pca.id_calendar_dim_begin) and 
+			isnull(dbo.IntDate_to_CalDate(pca.id_calendar_dim_end), dateadd(yy, 17.5 , birthdate))
+	join placement_care_auth_dim pcad
+		on pca.id_placement_care_auth_dim = pcad.id_placement_care_auth_dim
+			and pcad.cd_placement_care_auth in (1,2,7,8,9)
+	left join (select 
+					min(id_calendar_dim_plan_date) first_plan_act
+					,id_prsn_child 
+				from 
+					ca_ods.dbo.EDUCATION_PLAN_FACT epf
+						join ca_ods.dbo.people_dim pd 
+							on pd.id_prsn = epf.id_prsn_child 
+							and id_calendar_dim_plan_date <= convert(int, convert(varchar, dateadd(yy, 18, dt_birth),112))
+						group by 
+							id_prsn_child) epf
+		on rp.child = epf.id_prsn_child 
+	join calendar_dim cd 
+		on cd.CALENDAR_DATE = dateadd(yy, 17.5, birthdate)
+where 
+	--remove erroneous placements or fc to 21 placements
+	dbo.fnc_datediff_yrs(birthdate, removal_dt) < 18
+	--select a 17.5 year old cohort over our observation window
+	and dateadd(yy, 17.5, birthdate) between  '2011-07-01' and  '2014-06-30'
+	--where kids are still in care on their 17.5 bday 
+	and discharge_dt > dateadd(yy, 17.5, birthdate)
+	--where kids are in for 30 days on their 17.5 bday
+	and datediff(dd, removal_dt, dateadd(yy, 17.5, birthdate)) >= 30 
+group by 
+	child 
+	,removal_dt
+	,discharge_dt
+	,birthdate 
+	,dbo.fnc_datediff_yrs(birthdate, removal) 
+	,dateadd(yy, 17.5, birthdate) 
+	,epf.id_prsn_child
+	,epf.first_plan_act 
+	,state_fiscal_yyyy 
+	,rp.cd_region
+order by id_prsn
