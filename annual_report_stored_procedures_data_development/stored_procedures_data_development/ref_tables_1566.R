08a1cf5f7fd71a811eca144e34d6e2c7b90c5ee7
oieeri26 <oieeri26@gmail.com>
2015-05-21 14:13:24 -0700

    updated ref table script

--- annual_report_stored_procedures_data_development/stored_procedures_data_development/ref_tables_1566.R
+++ annual_report_stored_procedures_data_development/stored_procedures_data_development/ref_tables_1566.R
@@ -9,9 +9,9 @@ source("configurations.R")
 #### WARNING THIS DATA WAS CHANGED ####
 #######################################
 
-cd_disability <- c(0, 1, 2)
-tx_disibility <- c("All", "Non-Disabled", "Disabled")
-ref_lookup_disability <- as.data.frame(cbind(tx_disibility, cd_disability))
+fl_disability <- c(0, 1)
+tx_disibility <- c("Non-Disabled", "Disabled")
+ref_lookup_disability <- as.data.frame(cbind(fl_disability, tx_disibility))
 
 #########################################
 # Creating student typw reference table #
@@ -21,8 +21,8 @@ ref_lookup_disability <- as.data.frame(cbind(tx_disibility, cd_disability))
 #########################################
 
 cd_student_type <- c(0, 1, 2, 3)
-tx_student_type <- c("All", "Non_Foster_Care", "FRL_Non_Foster_Care", "Foster_Care")
-ref_lookup_student_type <- as.data.frame(cbind(tx_student_type, cd_student_type))
+tx_student_type <- c("All", "Not in Out-of-Home Care", "Not in Care: Receiving Free or Reduced-Price Lunch", "In Out-of-Home Care")
+ref_lookup_student_type <- as.data.frame(cbind(cd_student_type, tx_student_type))
 
 ######################################
 #### LOADING OR DROPPING THE DATA ####
@@ -31,5 +31,5 @@ ref_lookup_student_type <- as.data.frame(cbind(tx_student_type, cd_student_type)
 # sqlDrop(con_test_annie, sqtable = "ref_lookup_student_type")
 # sqlDrop(con_test_annie, sqtable = "ref_lookup_disability")
 
-sqlSave(con_test_annie, dat = ref_lookup_student_type, tablename = "dbo.ref_lookup_student_type", rownames = FALSE)
-sqlSave(con_test_annie, dat = ref_lookup_disability, tablename = "dbo.ref_lookup_disability", rownames = FALSE)
+sqlSave(con_test_annie, dat = ref_lookup_student_type, tablename = "ref_lookup_student_type", rownames = FALSE)
+sqlSave(con_test_annie, dat = ref_lookup_disability, tablename = "ref_lookup_disability", rownames = FALSE)
